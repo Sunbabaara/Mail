a051133e66c1c63c17b2f98a9e27048000d26343
Sunbabaara <csunbabaara@gmail.com>
2022-01-29 19:31:33 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header"
+import Sidebar from "./components/Sidebar"
+import Mail  from "./components/Mail";
+import EmailList from "./components/EmailList";
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Header/>
+
+      <div className="app_body">
+      <Sidebar/>
+      <Switch>
+       <Route path="/mail">
+        <Mail/>
+       </Route>
+
+       <Route path="/">
+        <EmailList/>
+       </Route>
+      </Switch>
+      </div>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
