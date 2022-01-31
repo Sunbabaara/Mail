8d4d1026f8302be4470ddcf3ae25bd8e39ce23f3
Sunbabaara <csunbabaara@gmail.com>
2022-01-31 01:26:31 -0500

    added new changes

--- src/components/Mail.js
+++ src/components/Mail.js
@@ -5,14 +5,17 @@ import MoveToInboxIcon from '@material-ui/icons/MoveToInbox';
 import ErrorIcon from '@material-ui/icons/Error';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { useHistory } from 'react-router-dom';
+import LabelImportantIcon  from '@material-ui/icons/LabelImportant';
 
 
 function Mail() {
+  const history = useHistory()
   return (
   <div className='mail'>
     <div className='mail_tools'>
 <div className='mail_toolsLeft'> 
-<IconButton>
+<IconButton onClick={()=>history.push("/")}>
   <ArrowBackIcon/>
 </IconButton>
 
@@ -33,7 +36,18 @@ function Mail() {
 </IconButton>
 </div>
     </div>
-    <h1>Whats up Tina</h1>
+    <div className='mail_body'>
+      <div className='mail_bodyHeader'>
+<h2>Subject</h2>
+<LabelImportantIcon className="mail_important"/>
+<p>Title</p>
+<p>10pm</p>
+      </div>
+      <div className="mail_message">
+        This is a message
+      </div>
+
+    </div>
   </div>
   )
 };
